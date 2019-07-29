aba9b159dca26a38ac0314f1b09bb51127de6d9f
xubo <xubo@yiguo.com>
2019-07-29 11:04:19 +0800

    update

---
+++ src/ElasticSearchNetDemo/ElasticSearchDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ElasticSearchNetDemo {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,world!");
+        }
+    }
+}
